b3d444ccec625fb19cfb8da44bde04345fe11997
Felix Bröhl <broehl@glamus.de>
2020-06-29 15:48:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# modularcms-cabrare-theme
+# Cabrare theme for modularcms
+
+This is the theme repository for the cabrare theme. You can easily add this theme to modularcms by downloading and importing the following file: [theme.json](theme.json)
+
+Feel free to fork this theme and create your own version.
+
+## Preview of the theme
+
+Will be available soon.
+
---
+++ cabrare_theme/ccm.cabrare_theme.js
@@ -0,0 +1,32 @@
+/**
+ * @overview ccm component for "cabrare" theme
+ * @author Felix Bröhl <broehl@everoo.io> 2020
+ * @license The MIT License (MIT)
+ */
+
+( () => {
+
+    const component = {
+
+        name: 'cabrare_theme',
+
+        ccm: 'https://ccmjs.github.io/ccm/versions/ccm-25.5.3.js',
+
+        config: {
+            "theme_helper": [ "ccm.instance", "https://modularcms.github.io/modularcms-components/theme_helper/versions/ccm.theme_helper.js" ]
+        },
+
+        Instance: function () {
+
+            this.start = async () => {
+
+                this.element.innerHTML = 'Hello, World!';
+
+            };
+
+        }
+
+    };
+
+    let b="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[b])return window.ccm.files[b]=component;(b=window.ccm&&window.ccm.components[component.name])&&b.ccm&&(component.ccm=b.ccm);"string"===typeof component.ccm&&(component.ccm={url:component.ccm});let c=(component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/)||["latest"])[0];if(window.ccm&&window.ccm[c])window.ccm[c].component(component);else{var a=document.createElement("script");document.head.appendChild(a);component.ccm.integrity&&a.setAttribute("integrity",component.ccm.integrity);component.ccm.crossorigin&&a.setAttribute("crossorigin",component.ccm.crossorigin);a.onload=function(){window.ccm[c].component(component);document.head.removeChild(a)};a.src=component.ccm.url}
+} )();
