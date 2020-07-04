7ba3d748749d3d67748ec062e9d74dd2633384b4
Felix Bröhl <broehl@glamus.de>
2020-07-04 03:00:21 +0200

    Fix row_with_columns_layout

--- row_with_columns_block/ccm.row_with_columns_block.js
+++ row_with_columns_block/ccm.row_with_columns_block.js
@@ -20,25 +20,29 @@
                 "https://modularcms.github.io/modularcms-cabrare-theme/row_with_columns_block/resources/css/style.css"
             ],
             "core": [ "ccm.instance", "https://modularcms.github.io/modularcms-components/theme_component_core/versions/ccm.theme_component_core-1.0.0.js" ],
+            "columns": 1
         },
 
         Instance: function () {
             let $;
+
+            const maxColumns = 4;
 
             this.ready = async () => {
                 $ = Object.assign( {}, this.ccm.helper, this.helper );                 // set shortcut to help functions
             };
 
             this.start = async () => {
-                let html = this.html.oneColumn;
-                if (this.columns == 2) {
-                    html = this.html.twoColumns;
-                } else if (this.columns == 3) {
-                    html = this.html.threeColumns;
-                } else if (this.columns == 4) {
-                    html = this.html.fourColumns;
+                let columnsDiv = document.createElement('div');
+                for (let i = 0; i < Math.min(this.columns, maxColumns); i++) {
+                    $.append(columnsDiv, $.html(this.html.column, {
+                        id: i+1
+                    }));
                 }
-                await this.core.initContent(html);
+
+                await this.core.initContent(this.html.main, {}, {
+                    'columns-container': columnsDiv
+                });
             };
 
         }
